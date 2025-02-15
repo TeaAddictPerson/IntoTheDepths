bef668b872cc5c16ddd552995a461716ec5d5a61
TeaAddictPerson <maksimovna2006@gmail.com>
2025-02-15 15:43:36 +0300

    PlayerAnims & SuperLowScript

---
+++ Into the Depth/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveSpeed=5f;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+direction*moveSpeed*Time.fixedDeltaTime);
+    }
+}
