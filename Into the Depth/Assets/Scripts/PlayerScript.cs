7d334ec80c12d3f88cbc4455bd3b8570bf4c5a7a
TeaAddictPerson <maksimovna2006@gmail.com>
2025-02-17 23:52:34 +0300

    Players Swim

--- Into the Depth/Assets/Scripts/PlayerScript.cs
+++ Into the Depth/Assets/Scripts/PlayerScript.cs
@@ -9,25 +9,25 @@ public class PlayerScript : MonoBehaviour
     private Vector2 direction;
     private Animator animator;
 
-    public Transform waterSurfacePoint; 
+    public Transform waterSurfacePoint;
     public Transform jumpCheckPoint;
-    public LayerMask raftLayer; 
+    public LayerMask raftLayer;
 
     private bool isOnSurface;
     private bool isJumping;
-    private bool isOnRaft; 
+    private bool isOnRaft;
     private float defaultGravityScale = 2f;
-    private float underwaterGravityScale = 0f; 
+    private float underwaterGravityScale = 0f;
 
-    public float jumpForce = 10f; 
+    public float jumpForce = 10f;
 
-    private bool FacingRight = true; 
+    private bool FacingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        rb.gravityScale = defaultGravityScale; 
+        rb.gravityScale = defaultGravityScale;
     }
 
     void Update()
@@ -36,44 +36,52 @@ public class PlayerScript : MonoBehaviour
         direction.y = Input.GetAxisRaw("Vertical");
 
         isOnSurface = transform.position.y >= waterSurfacePoint.position.y;
-
-
         bool isInWater = transform.position.y <= waterSurfacePoint.position.y;
-
 
         if (isOnSurface && direction.y > 0 && !isJumping)
         {
             direction.y = 0;
         }
 
-
         if (transform.position.y > waterSurfacePoint.position.y)
         {
- 
             rb.gravityScale = defaultGravityScale;
         }
         else
         {
-     
-            rb.gravityScale = underwaterGravityScale; 
+            rb.gravityScale = underwaterGravityScale;
         }
 
-
-        bool canJump = Physics2D.OverlapCircle(jumpCheckPoint.position, 0.1f, raftLayer); 
-
+        bool canJump = Physics2D.OverlapCircle(jumpCheckPoint.position, 0.1f, raftLayer);
 
         if (Input.GetKeyDown(KeyCode.Space) && (isOnRaft || isOnSurface) && canJump)
         {
             StartCoroutine(Jump());
         }
 
-  
         if (isInWater && !isJumping)
         {
-            animator.Play("PlayerIdle");
+            if (direction.magnitude > 0)
+            {
+                if (direction.y > 0)
+                {
+                    animator.Play("PlayerSwimUp");
+                }
+                else if (direction.y < 0)
+                {
+                    animator.Play("PlayerSwimDown");
+                }
+                else
+                {
+                    animator.Play("PlayerSwim");
+                }
+            }
+            else
+            {
+                animator.Play("PlayerIdle");
+            }
         }
 
-    
         if (direction.x < 0 && FacingRight)
         {
             Flip();
@@ -83,13 +91,12 @@ public class PlayerScript : MonoBehaviour
             Flip();
         }
 
-
         animator.SetBool("isOnSurface", isOnSurface);
     }
 
     void FixedUpdate()
     {
-        if (!isJumping) 
+        if (!isJumping)
         {
             rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
         }
@@ -100,16 +107,16 @@ public class PlayerScript : MonoBehaviour
         isJumping = true;
         animator.Play("PlayerJump");
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        yield return new WaitForSeconds(0.5f); 
+        yield return new WaitForSeconds(0.5f);
         isJumping = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Raft")) 
+        if (collision.gameObject.CompareTag("Raft"))
         {
             isOnRaft = true;
-            
+
             if (!isJumping)
             {
                 animator.Play("PlayerIdle");
@@ -119,7 +126,7 @@ public class PlayerScript : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Raft")) 
+        if (collision.gameObject.CompareTag("Raft"))
         {
             isOnRaft = false;
             rb.gravityScale = defaultGravityScale;
@@ -128,9 +135,9 @@ public class PlayerScript : MonoBehaviour
 
     private void Flip()
     {
-        FacingRight = !FacingRight; 
+        FacingRight = !FacingRight;
         Vector3 theScale = transform.localScale;
-        theScale.x *= -1; 
+        theScale.x *= -1;
         transform.localScale = theScale;
     }
 }
