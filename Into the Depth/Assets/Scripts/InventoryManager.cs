a2b761ef74ed0ad3ba7c7d95757935e13743d955
TeaAddictPerson <maksimovna2006@gmail.com>
2025-02-22 17:14:54 +0300

    HalfStackInventory

--- Into the Depth/Assets/Scripts/InventoryManager.cs
+++ Into the Depth/Assets/Scripts/InventoryManager.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private GameObject slotHolder;
     [SerializeField] private ItemsClass itemToAdd;
     [SerializeField] private ItemsClass itemToRemove;
-  public List<ItemsClass> items = new List<ItemsClass>();
+  public List<SlotClass> items = new List<SlotClass>();
 
   private GameObject[] slots;
 
@@ -24,33 +25,64 @@ public class InventoryManager : MonoBehaviour
   }
 
   public void RefreshUI()
-  {
-
-    for(int i=0; i<slots.Length; i++)
+{
+    for (int i = 0; i < slots.Length; i++)
     {
         try
         {
-            slots[i].transform.GetChild(0).GetComponent<Image>().enabled=true;
-            slots[i].transform.GetChild(0).GetComponent<Image>().sprite=items[i].itemIcon;
+            slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+            slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;     
+            slots[i].transform.GetChild(1).GetComponent<TMP_Text>().text = items[i].GetQuantity() + ""; 
         }
         catch
         {
-            slots[i].transform.GetChild(0).GetComponent<Image>().sprite=null;
-            slots[i].transform.GetChild(0).GetComponent<Image>().enabled=false;
+            slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+            slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+            slots[i].transform.GetChild(1).GetComponent<TMP_Text>().text = ""; 
         }
-        
     }
-  }
+}
 
   public void Add(ItemsClass item)
   {
-    items.Add(item);
+ 
+    SlotClass slot = Contains(item);
+        if(slot!=null)
+    slot.AddQuantity(1);
+    else
+    {
+        items.Add(new SlotClass(item,1));
+    }
+
     RefreshUI();
   }
 
   public void Remove(ItemsClass item)
   {
-    items.Remove(item);
+ 
+    SlotClass slotToRemove = new SlotClass();
+
+    foreach(SlotClass slot in items)
+    {
+        if(slot.GetItem()==item)
+        {
+            slotToRemove = slot;
+            break;
+        }
+    }
+    items.Remove(slotToRemove);
+
     RefreshUI();
+  }
+
+  public SlotClass Contains(ItemsClass item)
+  {
+     foreach(SlotClass slot in items)
+    {
+        if(slot.GetItem() == item)
+        return slot;
+    }
+
+    return null;
   }
 }
---
+++ Into the Depth/Assets/Scripts/SlotClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotClass 
+{
+    private ItemsClass item;
+    private int quantity;
+
+    public SlotClass()
+    {
+        item=null;
+        quantity=0;
+    }
+    public SlotClass(ItemsClass _item, int _quantity)
+    {
+        item=_item;
+        quantity=_quantity;
+    }
+
+    public ItemsClass GetItem() { return item; }
+    public int GetQuantity() { return quantity; }
+    public void AddQuantity(int _quantity) { quantity += _quantity;}
+}
